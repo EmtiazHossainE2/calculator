a5a18b49659c75b1fd36d47d0e500901ccfda557
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-14 05:08:25 +0600

    js code added

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('linked');
+let outputDisplay = document.getElementById('display');
+
+
+// Display 
+function display(num) {
+    outputDisplay.value += num;
+}
+
+// Clear 
+function Clear() {
+    outputDisplay.value = '';
+}
+
+// Delete 
+function Delete() {
+    outputDisplay.value = outputDisplay.value.slice(0, -1)
+}
+
+// Calculate Function
+function calculate() {
+    try {
+        outputDisplay.value = eval(outputDisplay.value)
+    }
+
+    catch (err) {
+        alert('Invalid')
+    }
+
+
+}
